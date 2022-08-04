36ca8072b864eef875d1145cc0854feadf2beebe
ErickGX <ericksaopaulino1@hotmail.com>
2022-08-03 22:18:25 -0300

    Começando denovo

---
+++ app/src/main/java/br/com/etecia/atvmockupmob3/HelloActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atvmockupmob3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+}
